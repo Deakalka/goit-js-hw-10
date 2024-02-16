332b7fdcf927217fb41278587b4408b857079d80
Deakalka <fedorushun.borya@gmail.com>
2024-02-17 01:29:15 +0200

    1

---
+++ src/js/1-timer.js
@@ -0,0 +1,125 @@
+
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const input = document.querySelector('input[type="text"]');
+const button = document.querySelector('button[type="button"]');
+const dataDays = document.querySelector('[data-days]');
+const dataHours = document.querySelector('[data-hours]');
+const dataMin = document.querySelector('[data-minutes]');
+const dataSec = document.querySelector('[data-seconds]');
+
+button.disabled = true; 
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      if (selectedDates[0] < new Date()) {
+          iziToast.error({
+              message: 'Please choose a date in the future',
+              position: "topRight",
+});
+        updateStartButton(false);
+      } else {
+        updateStartButton(true); 
+        updateStateInput(false);
+          userSelectedDate = selectedDates[0];
+        }
+    },     
+};
+
+let userSelectedDate;
+
+const fp = new flatpickr('input', options);
+
+class Timer {
+    constructor(updateDisplay) {
+        this.updateDisplay = updateDisplay;
+        this.isActive = false;
+    }
+    start() {
+        if (this.isActive) return;
+        this.isActive = true;
+ updateStateInput(true);
+        this.intervalId = setInterval(() => {
+             this.updateTime();
+        }, 1000);
+  }
+  
+  stop() {
+        clearInterval(this.intervalId);
+        this.isActive = false;
+        updateStartButton(true);
+        updateStateInput(false);
+    }
+
+    updateTime() {
+    const target = userSelectedDate.getTime();
+      const now = Date.now();
+      const diff = target - now;
+        const timeObj = convertMs(diff);
+    if (diff <= 1000) {
+       this.stop();
+      
+    }
+      updateStartButton(false);
+    this.updateDisplay(timeObj);
+  }  
+}
+
+const timer = new Timer(updateDisplay);
+
+button.addEventListener('click', () => {
+    timer.start();
+   
+});
+
+ function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+function updateDisplay({days, hours, minutes, seconds}) {
+    const formattedDays = addZero(days); 
+    const formattedHours = addZero(hours); 
+    const formattedMin = addZero(minutes);
+    const formattedSec = addZero(seconds);
+    dataDays.textContent = formattedDays;
+    dataHours.textContent = formattedHours;
+    dataMin.textContent = formattedMin;
+    dataSec.textContent = formattedSec;
+}
+
+function addZero(num) {
+    return num.toString().padStart(2, '0');
+}
+
+function updateStartButton(isActive) {
+  button.disabled = !isActive;
+}
+
+function updateStateInput(isActive) {
+  input.disabled = isActive;
+}
